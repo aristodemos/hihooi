6112db8f2e4f24a78cd208a76477ec6b01c05850
Ubuntu <a.rpiko@gmail.com>
2015-10-10 15:23:56 +0000

    pass number of threads as a parameter using -s

--- arisDemo.java
+++ arisDemo.java
@@ -196,10 +196,10 @@ public class arisDemo {
 	public static void main(String [] args) throws IOException {
 		if (args.length > 0) {
 			for (int i=0; i< args.length; i++) {
-				/*if (args[i].equalsIgnoreCase("-sessions") ||args[i].equalsIgnoreCase("-s")) {
-				*	SESSIONS = Integer.parseInt(args[i + 1]);
-				*}
-				*
+				if (args[i].equalsIgnoreCase("-sessions") ||args[i].equalsIgnoreCase("-s")) {
+					SESSIONS = Integer.parseInt(args[i + 1]);
+				}
+				/*
 				*if (args[i].equalsIgnoreCase("-txns") || args[i].equalsIgnoreCase("-t")) {
 				*	trxnsPerSession= Integer.parseInt(args[i + 1]);
 				*}
