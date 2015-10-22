f316d1730e1d86d4e4d45982adb199c605a855e8
Aristodemos <a.rpiko@gmail.com>
2015-10-22 20:52:41 +0300

    changes have been made to bypass hihooi - We have to undo review them before start working with hihooi again

--- arisDemo.java
+++ arisDemo.java
@@ -42,8 +42,8 @@ public class arisDemo {
 		}
 	}
 
-	private String LISTENER="52.26.152.181";
-	//private String LISTENER="172.30.0.206";
+	//private String LISTENER="52.26.152.181";
+	private String LISTENER="172.30.0.206";
 	private HiHListenerClient hih = new HiHListenerClient();
 
 	public String CONNECT()
